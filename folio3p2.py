75e4477944753f3f24d25dde3fb9a40c2f6c557d
stevenhuyn <stevenhuyn@gmail.com>
2017-03-10 09:54:15 +1100

    updated folio3p2.py

--- folio3p2.py
+++ folio3p2.py
@@ -6,11 +6,11 @@ import random
 
 def generate():
     nodeList = list(range(31))
-    edgeList = ( (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8),
-                 (0, 9), (1, 10), (2, 11), (2, 12), (2, 13), (2, 14), (3, 15),
-                 (3, 16), (3, 17), (3, 18), (4, 19), (4, 20), (5, 21), (6, 22),
-                 (6, 23), (7, 24), (7, 25), (7, 26), (8, 27), (8, 28), (9, 29),
-                 (9, 30))
+    edgeList = ((0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8),
+                (0, 9), (1, 10), (2, 11), (2, 12), (2, 13), (2, 14), (3, 15),
+                (3, 16), (3, 17), (3, 18), (4, 19), (4, 20), (5, 21), (6, 22),
+                (6, 23), (7, 24), (7, 25), (7, 26), (8, 27), (8, 28), (9, 29),
+                (9, 30))
     
     G = nx.Graph()
     G.add_nodes_from(nodeList)
@@ -19,7 +19,9 @@ def generate():
     return G
 
 def sim1(G, u, v):
-    # Question 1
+    """ Question 1
+    u, v are staff nodes that are not connected by the same phone.
+    """
     nx.set_edge_attributes(G, 'color', 'grey')
     nx.set_edge_attributes(G, 'width', 0.5)
     yield True
@@ -37,6 +39,7 @@ def sim1(G, u, v):
     G.edge[0][firstPhone]['width'] = 2
     yield True
 
+    # Checking phone neighbours of PBX to see if the phone contains v
     for phone in G.neighbors(0):
         if v in G.neighbors(phone):
             G.node[phone]['color'] = 'yellow'
@@ -54,14 +57,20 @@ def sim1(G, u, v):
     yield False
 
 def sim2(G, v):
-    # Question 2
+    """ Question 2
+    v is the node that will be connected to
+
+    A cut and paste of simulation but removes the initial part of connecting
+    to the PBX from u
+    """
     nx.set_edge_attributes(G, 'color', 'grey')
     nx.set_edge_attributes(G, 'width', 0.5)
     yield True
 
     G.node[0]['color'] = 'yellow'
     yield True
-    
+
+    # Checking phone neighbours of PBX to see if the phone contains v
     for phone in G.neighbors(0):
         if v in G.neighbors(phone):
             G.node[phone]['color'] = 'yellow'
@@ -78,12 +87,29 @@ def sim2(G, v):
 
     yield False
 
-if __name__ == '__main__':
+def onPress(event):
+    # Just for ease of use
+    if event.key == 'ctrl+d':
+        exit()
+    elif event.key == 'ctrl+c':
+        global step
+        step = False
+    elif event.key == 'ctrl+z':
+        pylab.cla()
+        main()
+
+def main():
+    pylab.gcf().canvas.mpl_connect('key_press_event', onPress)
+    
     G = generate()
 
+    # Randomly selects 2 staff nodes
     u, v = tuple(random.sample(list(range(10, 31)), 2))
-    
+
+    # Swap out in the for loop to see either simulation
     genA, genB = sim1(G, u, v), sim2(G, v)
+
+    # Adding a nice title to the graph
     strA = 'Connecting ' + str(u) + ' to ' + str(v)
     strB = 'External call to ' + str(v)
 
@@ -91,6 +117,9 @@ if __name__ == '__main__':
     for keepGoing in genA:
         pylab.title(strA)
         show(G, setPos=position)
-        pylab.pause(0.3)
+        pylab.pause(0.0001)
         if keepGoing != False:
             pylab.cla()
+        
+if __name__ == '__main__':
+    main()
